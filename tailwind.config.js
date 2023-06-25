a9e95f9768d843f40aa8a68a50cfc4a248a43933
Theophilus <isahtheophilus@gmail.com>
2023-06-25 05:42:12 +0100

    feat: add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      main: ['Lobster', 'cursive'],
+      serif: ['Montserrat', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
